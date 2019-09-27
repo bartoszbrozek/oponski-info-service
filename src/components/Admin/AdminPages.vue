aabd866f33e861fb79a949779ad3a580a56ad6c0
bartoszbrozek <bartekbrozek@gmail.com>
2019-09-27 16:26:40 +0200

    Frontend fixes

--- src/components/Admin/AdminPages.vue
+++ src/components/Admin/AdminPages.vue
@@ -2,40 +2,48 @@
   <div>
     <div class="container is-fluid" v-if="isLoggedIn">
       <!-- NAVBAR -->
-      <b-navbar class="is-primary">
-        <template slot="brand">
-          <b-navbar-item tag="router-link" :to="{ path: '/' }">
-            <img src="./../../assets/logo.png" alt="OPOŃ" />
-          </b-navbar-item>
-        </template>
-        <template slot="start">
-          <b-navbar-item href="#">Home</b-navbar-item>
-        </template>
+      <section>
+        <b-navbar class="is-primary">
+          <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img src="./../../assets/logo.png" alt="OPOŃ" />
+            </b-navbar-item>
+          </template>
+          <template slot="start">
+            <b-navbar-item href="#">Home</b-navbar-item>
+          </template>
 
-        <template slot="end">
-          <b-navbar-item tag="div">
-            <div class="buttons">
-              <a class="button is-light" @click="logout">Log out</a>
-            </div>
-          </b-navbar-item>
-        </template>
-      </b-navbar>
+          <template slot="end">
+            <b-navbar-item tag="div">
+              <div class="buttons">
+                <a class="button is-light" @click="logout">Log out</a>
+              </div>
+            </b-navbar-item>
+          </template>
+        </b-navbar>
+      </section>
 
       <div class="container">&nbsp;</div>
 
       <div class="columns">
-        <div class="column is-one-fifth">
+        <div class="column is-narrow">
           <!-- MENU -->
-          <b-menu>
-            <b-menu-list label="Menu">
-              <b-menu-item label="Dashboard" tag="router-link" :to="{ path: '/admin' }"></b-menu-item>
-              <b-menu-item label="News" tag="router-link" :to="{ path: '/admin/news' }"></b-menu-item>
-              <b-menu-item label="Categories" tag="router-link" :to="{ path: '/admin/categories' }"></b-menu-item>
-            </b-menu-list>
-            <b-menu-list label="Actions">
-              <b-menu-item label="Logout" @click="logout"></b-menu-item>
-            </b-menu-list>
-          </b-menu>
+          <section>
+            <b-menu>
+              <b-menu-list label="Menu">
+                <b-menu-item label="Dashboard" tag="router-link" :to="{ path: '/admin' }"></b-menu-item>
+                <b-menu-item label="News" tag="router-link" :to="{ path: '/admin/news' }"></b-menu-item>
+                <b-menu-item
+                  label="Categories"
+                  tag="router-link"
+                  :to="{ path: '/admin/categories' }"
+                ></b-menu-item>
+              </b-menu-list>
+              <b-menu-list label="Actions">
+                <b-menu-item label="Logout" @click="logout"></b-menu-item>
+              </b-menu-list>
+            </b-menu>
+          </section>
         </div>
 
         <div class="column">
